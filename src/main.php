2b0311a1990d2e532e8de7d55ce5d587dc2e9d78
Zomato <zomato@ZomAdmins-MacBook-Air-5.local>
2014-12-19 12:14:30 +0530

    php script, config

--- src/main.php
+++ src/main.php
@@ -1,13 +1,21 @@
 <?php
-
+date_default_timezone_set("UTC");
 class Parser {
 	public function __construct() {
-		$this->baseDir = "/Users/zomato/tw/";
+		$this->config = parse_ini_file("config", true);
+		$this->baseDir = $this->config["paths"]["base_directory"];
 		$this->citiesDict = array();
+		$this->db = new mysqli($this->config["database"]["server"], $this->config["database"]["username"], $this->config["database"]["password"], $this->config["database"]["dbname"]);
+		$this->db->autocommit(true);
+		$result = $this->db->query("SELECT timestamp FROM latest_timestamp LIMIT 1");
+		$row = $result->fetch_assoc();
+		$this->latestTimestamp = $row['timestamp'];
+		$this->newTimestamp = $this->latestTimestamp;
+		$this->batchSize = 300;
 	}
 
 	public function loadCities() {
-		$handle = fopen($this->baseDir . "cities.csv", "r");
+		$handle = fopen($this->baseDir . $this->config["paths"]["relative_citiesCSV"], "r");
 		$data = fgetcsv($handle, 1000, ",");
 		while( ($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 			$this->citiesDict[substr($data[2], 0, strlen($data[2])-1)] = $data[0];
@@ -15,26 +23,42 @@ class Parser {
 	}
 
 	public function parseLog($fname) {
-		$handle = fopen($this->baseDir . $fname, "r");
+		$handle = gzopen($fname, "r");
 		$logs = array();
 		$tc = $vc = 0;
 		$this->dist = array();
-		// var_dump($this->citiesDict);
-		while( ($data = fgets($handle)) !== FALSE) {
+		while( ($data = gzgets($handle)) !== FALSE) {
+			$tc += 1;
+			// if ($tc > 10000) {
+			// 	break;
+			// }
 			$line = $data;
+			$dateReg = preg_match('/\[(\d{1,2}\/[a-zA-Z]{3}\/\d{4}:\d{2}:\d{2}:\d{2})\s[+-]\d{4}\]/', $line, $dateMatches);
 			$ipReg = preg_match("/\s((\d{0,3}\.){3}\d{0,3})\s/", $line, $ipMatches);
 			$urlReg = preg_match("/(GET|POST)\s\/(.*?)\//", $line, $urlMatches);
-			if (!isset($ipMatches[1]) || !isset($urlMatches[2])) {
+			if (!isset($ipMatches[1]) || !isset($urlMatches[2]) || !isset($dateMatches[1])) {
 				continue;
 			}
 			$ip = $ipMatches[1];
 			$url = $urlMatches[2];
+			$date = $dateMatches[1];
+
 			$ipBlock = substr($ip, 0, strlen($ip)-strpos(strrev($ip), ".")-1);
-			// echo "\n" . $tc;
-			// $tc += 1;
-			// continue;
+
+			if ($ipBlock == "127.0.0") {
+				continue;
+			}
 			if (array_key_exists($url, $this->citiesDict)) {
-				echo "\n" .$vc . ". " . $ipBlock . " ==> " . $url . " (" . $this->citiesDict[$url] . ")";
+
+				$date_components = explode("/", $date);
+				$date_components[1] = $this->getMonthInt($date_components[1]);
+				$time_components = $this->castArrayToInt(explode(":", $date_components[2]));
+				$date_components = $this->castArrayToInt($date_components);
+				$this->newTimestamp = mktime($time_components[1], $time_components[2], $time_components[3], $date_components[1], $date_components[0], $time_components[0]);
+				if ($this->newTimestamp <= $this->latestTimestamp) {
+					continue;
+				}
+				// echo "\n" .$vc . ". " . $ipBlock . " ==> " . $url . " (" . $date . ")";
 				$vc += 1;
 				$city = $this->citiesDict[$url];
 				if (!array_key_exists($ipBlock, $this->dist)) {
@@ -46,31 +70,39 @@ class Parser {
 					$this->dist[$ipBlock][$city] += 1;
 				}
 			}
-			$tc += 1;
-			// if ($tc > 50) {
-			// 	break;
-			// }
+			
 		}
 	}
 
 	public function updateDB() {
-		$this->db = new mysqli("127.0.0.1", "zomato", "zomato", "zomato_geoip");
-		$this->db->autocommit(false);
+		$queries = array();
 		foreach ($this->dist as $ipBlock => $dist) {
-			$result = $this->db->query("SELECT * FROM geoip_mapping WHERE ip_block='$ipBlock' FOR UPDATE");
+			$result = $this->db->query("SELECT * FROM geoip_mapping WHERE ip_block='$ipBlock'");
 			if ($result->num_rows > 0) {
 				$row = $result->fetch_assoc();
 				$newJSON = $this->mergeJSON(json_decode($row['distribution_json'], true), $this->dist[$ipBlock]);
 				$topCities = $this->getTopCities($newJSON, 3);
-				$this->db->query("UPDATE geoip_mapping SET city_1=$topCities[0], city_2=$topCities[1], city_3=$topCities[2], distribution_json='" . json_encode($newJSON) ."' WHERE ip_block='$ipBlock'");
-				$this->db->commit();
+				$queries[] = "UPDATE geoip_mapping SET city_1=$topCities[0], city_2=$topCities[1], city_3=$topCities[2], distribution_json='" . json_encode($newJSON) ."' WHERE ip_block='$ipBlock'";
 			} else {
 				$topCities = $this->getTopCities($this->dist[$ipBlock], 3);
-				$this->db->query("INSERT INTO geoip_mapping(ip_block, city_1, city_2, city_3, distribution_json) VALUES('$ipBlock', $topCities[0], $topCities[1], $topCities[2], '" . json_encode($this->dist[$ipBlock]) ."')");
-				$this->db->commit();
+				$queries[] = "INSERT INTO geoip_mapping(ip_block, city_1, city_2, city_3, distribution_json) VALUES('$ipBlock', $topCities[0], $topCities[1], $topCities[2], '" . json_encode($this->dist[$ipBlock]) ."')";
+			}
+			if (count($queries) > $this->batchSize) {
+				if (!$this->db->multi_query(implode(";", $queries))) {
+					echo $this->db->error;
+				}
+				while ($this->db->next_result()) {;}
+				$queries = array();
 			}
 		}
-		$this->db->close();
+		
+		if (count($queries) > 0) {
+			if(!$this->db->multi_query(implode(";", $queries))) {
+				echo $this->db->error;
+			}
+			while ($this->db->next_result()) {;}
+		}
+		$this->db->query("UPDATE latest_timestamp SET timestamp=" . $this->newTimestamp);
 	}
 
 	public function mergeJSON($existingJSON, $newJSON) {
@@ -97,14 +129,51 @@ class Parser {
 		}
 		return $topCitiesKeys;
 	}
+
+	public function castArrayToInt($a) {
+		foreach ($a as $k => $v) {
+			$a[$k] = (int) $v;
+		}
+		return $a;
+	}
+
+	public function getMonthInt($month) {
+		$map = array();
+		$map['Jan'] = 0;
+		$map['Feb'] = 1;
+		$map['Mar'] = 2;
+		$map['Apr'] = 3;
+		$map['May'] = 4;
+		$map['Jun'] = 5;
+		$map['Jul'] = 6;
+		$map['Aug'] = 7;
+		$map['Sep'] = 8;
+		$map['Oct'] = 9;
+		$map['Nov'] = 10;
+		$map['Dec'] = 11;
+		return $map[$month];
+	}
+
+	public function processAllLogs() {
+		$files = glob($this->baseDir . $this->config["paths"]["relative_configFilesFormat"]);
+		$files_times = array();
+		foreach ($files as $f) {
+			$files_times[$f] = filemtime($f);
+		}
+		asort($files_times);
+		foreach ($files_times as $f => $t) {
+			echo "\nParsing log " . $f;
+			$this->parseLog($f);
+			echo "\nUpdating DB ";
+			$this->updateDB();
+		}
+		$this->db->close();
+	}
 }
 
 $p = new Parser();
 echo "\nLoading cities";
 $p->loadCities();
-echo "\nParsing log";
-$p->parseLog("web/access.log");
-echo "\n Updating DB";
-$p->updateDB();
+$p->processAllLogs();
 
 ?>
--- src/main.py
+++ src/main.py
@@ -9,84 +9,118 @@ import re
 import json
 import MySQLdb
 import operator
+from time import mktime
+from datetime import datetime
+import gzip
+import glob
+from ConfigParser import RawConfigParser
 
 class Parser:
 
 	def __init__(self):
-		self.baseDir = "/Users/zomato/tw/"
+		self.config = RawConfigParser()
+		self.config.read("config")
+		self.baseDir = self.config.get("paths", "base_directory")
 		self.citiesDict = {}
+		self.db = MySQLdb.connect(self.config.get("database", "server"), self.config.get("database", "username"), self.config.get("database", "password"), self.config.get("database", "dbname"))
+		cursor = self.db.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute("SELECT timestamp FROM latest_timestamp LIMIT 1")
+		self.latestTimestamp = cursor.fetchone()['timestamp']
+		cursor.close()
+		self.newTimestamp = self.latestTimestamp
+		self.batchSize = 300
 
 	def loadCities(self):
-		with open(os.path.join(self.baseDir, "cities.csv")) as f:
+		with open(os.path.join(self.baseDir, self.config.get("paths", "relative_citiesCSV"))) as f:
 			creader = csv.reader(f)
 			for row in creader:
 				self.citiesDict[row[2][:-1]] = row[0]
 
 	def parseLog(self, fname):
-		with open(os.path.join(self.baseDir, fname)) as f:
-			logs = f.readlines()
-
+		DateRegEx = re.compile(r'\[(\d{1,2}\/[a-zA-Z]{3}\/\d{4}:\d{2}:\d{2}:\d{2})\s[+-]\d{4}\]')
 		IPRegEx = re.compile(r'\s((\d{0,3}\.){3}\d{0,3})\s')
 		URLRegEx = re.compile(r'(GET|POST)\s/(.*?)/')
 		tc = 0
 		vc = 0
 		self.dist = {}
-		for line in logs:
+		previousDate = ""
+		f = gzip.open(fname, "r")
+		for line in f:
+			tc += 1
+			# if tc > 10000:
+			# 	break
 			try:
 				ip = IPRegEx.search(line).group(0).strip() # get IP
 				url = URLRegEx.search(line).group(2).strip() # get URL
-			except Exception:
+				dt = DateRegEx.search(line).group(1).strip() # get date
+			except Exception as e:
 				continue
 			ipBlock = ip[:ip.rfind(".")] # get first 3 bytes
+			if ipBlock == "127.0.0":
+				continue
 			if url in self.citiesDict:
-				print str(vc) + ". " + ipBlock + " ==> " + url + " (" + self.citiesDict[url] + ")"
+				self.newTimestamp = int(mktime(datetime.strptime(dt, "%d/%b/%Y:%H:%M:%S").timetuple()))
+
+				if self.newTimestamp <= self.latestTimestamp:
+					continue
+
+				# print str(vc) + ". " + ipBlock + " ==> " + url + " (" + self.citiesDict[url] +")"
 				vc += 1
 				city = self.citiesDict[url]
 				if ipBlock not in self.dist:
 					self.dist[ipBlock] = {}
-
 				if city not in self.dist[ipBlock]:
 					self.dist[ipBlock][city] = 1
 				else:
 					self.dist[ipBlock][city] += 1
-			tc += 1
-
-		print tc
-		# print json.dumps(dist)
+		f.close()
 
 	def updateDB(self):
-		self.db = MySQLdb.connect("localhost", "zomato", "zomato", "zomato_geoip")
-		self.cursor = self.db.cursor(MySQLdb.cursors.DictCursor)
+		insertData = []
+		updateData = []
+		count = 0
+		cursor = self.db.cursor(MySQLdb.cursors.DictCursor)
+
 		for ipBlock in self.dist:
-			
-			# Get an exclusive lock on the row
-			self.cursor.execute("SELECT * FROM geoip_mapping WHERE ip_block=%s FOR UPDATE", (ipBlock,))
-			doUpdate = 1
-			keyerror = 0
-			if self.cursor.rowcount == 0:
+			cursor.execute("SELECT * FROM geoip_mapping WHERE ip_block='%s'" % ipBlock)
+			if cursor.rowcount == 0:
 				topCities = self.getTopCities(self.dist[ipBlock], 3)
-				try:
-					doUpdate = 0
-					# Insert record
-					self.cursor.execute("INSERT INTO geoip_mapping(ip_block, city_1, city_2, city_3, distribution_json) VALUES(%s, %s, %s, %s, %s)", (ipBlock, topCities[0], topCities[1], topCities[2], json.dumps(self.dist[ipBlock])))
-					self.db.commit()
-				except Exception:
-					# The most likely exception to be raised is if some other instance inserts the same IP in the meanwhile, so we rollback
-					self.db.rollback()
-					doUpdate = 1
-					keyerror = 1
-			if doUpdate:
-				# keyerror=1 only if a duplicate was inserted by some other instance. so we get the latest record
-				if keyerror: 
-					self.cursor.execute("SELECT * FROM geoip_mapping WHERE ip_block=%s FOR UPDATE", (ipBlock,))
-				rows = self.cursor.fetchall()
+				insertData.append((ipBlock, topCities[0], topCities[1], topCities[2], json.dumps(self.dist[ipBlock])))
+			else:
+				rows = cursor.fetchall()
 				row = rows[0]
 				newJSON = self.mergeJSON(json.loads(row['distribution_json']), self.dist[ipBlock])
 				topCities = self.getTopCities(newJSON, 3)
-				self.cursor.execute("UPDATE geoip_mapping SET city_1=%s, city_2=%s, city_3=%s, distribution_json=%s WHERE ip_block=%s", (topCities[0], topCities[1], topCities[2], json.dumps(newJSON), ipBlock))
-				self.db.commit()
+				updateData.append((topCities[0], topCities[1], topCities[2], json.dumps(newJSON), ipBlock))
+			count += 1
+			if count > self.batchSize:
+				# Execute in batches
+				self.executeBatch(insertData, updateData)
+				count = 0
+				insertData = []
+				updateData = []
 
-		self.db.close()
+		# Execute residual batch (last batch < threshold)
+		if count > 0: 
+			self.executeBatch(insertData, updateData)
+
+		if self.newTimestamp is not None:
+			# print "updating timestamp .. " + str(self.newTimestamp)
+			cursor.execute("UPDATE latest_timestamp SET timestamp=%s", (self.newTimestamp,))
+			self.db.commit()
+		cursor.close()
+
+	def executeBatch(self, insertData, updateData):
+		if len(insertData) == 0 and len(updateData) == 0:
+			return
+		# print "Executing batch.."
+		cursor = self.db.cursor(MySQLdb.cursors.DictCursor)
+		insertQuery = "INSERT INTO geoip_mapping(ip_block, city_1, city_2, city_3, distribution_json) VALUES(%s, %s, %s, %s, %s)"
+		cursor.executemany(insertQuery, insertData)
+		updateQuery = "UPDATE geoip_mapping SET city_1=%s, city_2=%s, city_3=%s, distribution_json=%s WHERE ip_block=%s"
+		cursor.executemany(updateQuery, updateData)
+		self.db.commit()
+		cursor.close()
 
 	def mergeJSON(self, existingJSON, newJSON):
 		returnJSON = existingJSON
@@ -98,7 +132,6 @@ class Parser:
 		return returnJSON
 
 	def getTopCities(self, d, n):
-		# print d
 		topCities = sorted(d.items(), key=operator.itemgetter(1))[(-1*n):]
 		topCitiesKeys = [int(x[0]) for x in topCities]
 		topCitiesKeys.reverse()
@@ -108,11 +141,18 @@ class Parser:
 			i += 1
 		return topCitiesKeys
 
+	def processAllLogs(self):
+		files = filter(os.path.isfile, glob.glob(self.baseDir + self.config.get("paths", "relative_configFilesFormat")));
+		files.sort(key=lambda x: os.path.getmtime(x))
+		for f in files:
+			print "Parsing log " + f
+			self.parseLog(f)
+			print "Updating DB"
+			self.updateDB()
+		self.db.close()
+
 if __name__ == "__main__":
 	p = Parser()
 	print "Loading cities"
 	p.loadCities()
-	print "Parsing log"
-	p.parseLog("web/access.log")
-	print "Updating DB"
-	p.updateDB()
+	p.processAllLogs()
